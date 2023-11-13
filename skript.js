cf884d571936e4e36e38554c310ec889528ebf1e
theo <theo@zibell.se>
2023-11-13 16:38:40 +0100

    Got stuck on tag /id stuff

--- skript.js
+++ skript.js
@@ -2,61 +2,151 @@ console.log("hello js is working");
 
 let products = [
     {
-        name: "skruv",
-        pris: 100,
-        tag: "bygg",
+        name: "Träskruv FZB 6,0x100",
+        price: 169,
+        tag: "Bygg",
     },
     {
-        name: "Hammare",
-        pris: 100,
-        tag: "bygg",
+        name: "Hammare Stanley",
+        price: 99.95,
+        tag: "Bygg",
+    },
+    {
+        name: "Borrhammare MAKITA DHR202Z 18V utan batteri och laddare",
+        price: 1839,
+        tag: "Bygg",
+    },
+    {
+        name: "Stålhammare DEWALT rivklo slät 16oz",
+        price: 499,
+        tag: "Bygg",
+    },
+    {
+        name: "Foder vit 12x43x2200mm S 0502-Y",
+        price: 72.95,
+        tag: "Bygg",
+    },
+    {
+        name: "Himeji slott",
+        price: 1900,
+        tag: "Lego",
+    },
+    {
+        name: "Lejonriddarnas slott",
+        price: 4650,
+        tag: "Lego",
+    },
+    {
+        name: "Venator-Class Republic Attack Cruiser",
+        price: 7800,
+        tag: "Lego",
+    },
+    {
+        name: "Millennium Falcon",
+        price: 9800,
+        tag: "Lego",
+    },
+    {
+        name: "AT-AT",
+        price: 9800,
+        tag: "Lego",
     },
 ];
 
 
-function UppdateView() {
+function UppdateView(arrayOfPruducts) 
+{
+    let shop = document.querySelector(".shop");
+    shop.innerHTML = "";
 
+    for (let index = 0; index < arrayOfPruducts.length; index++) 
+    {
+        let divContener = document.createElement("div");
+        divContener.classList = "product";
+
+        let name = document.createElement("h3");
+        name.innerHTML = arrayOfPruducts[index].name;
+        divContener.append(name);
+
+        let price = document.createElement("h3");
+        price.innerHTML = arrayOfPruducts[index].price + "kr";
+        divContener.append(price);
+
+        let tag = document.createElement("h3");
+        tag.innerHTML = arrayOfPruducts[index].tag;
+        divContener.append(tag);  
+
+        shop.append(divContener);
+    }
 }
 
-function CreatBase() {
-    let body = document.querySelector("body");
-    let divShop = document.createElement("div");
-    divShop.classList = ".shop";
+function ChangeTag(event)
+{
+    switch (event.target.id) {
+        case "Lego":
+            console.log("1");
+            break;
+        case "Bygg": 
+            console.log("2");
+        break; 
 
+        default:
+            break;
+    }
+
+    console.log(event.target.id);
+}
+
+function CreatBase() 
+{
+    //creat div
+    let body = document.querySelector("body");
+    let divMain = document.createElement("div");
+    divMain.classList = "Main";
+    
+    //titel
     let h1 = document.createElement("h1");
     h1.innerHTML = "Shop";
-    divShop.append(h1);
+    divMain.append(h1);
 
+    //label
     let dropDownLabel = document.createElement("label");
-
+    dropDownLabel.innerHTML = "Sortera efter ";
+    divMain.append(dropDownLabel);
 
     //drop down
     let dropDown = document.createElement("select");
-
     dropDown.addEventListener("change", (event) => {
-
-
-
-        console.log(event.target.value);
+        ChangeTag(event);
     })
 
     let choice = document.createElement("option");
-    choice.innerHTML = "test";
-    choice.id = "TEST";
+    choice.innerHTML = " Tag Ingen";
+    choice.id = "Ingen";
     dropDown.append(choice);
 
-    let choice2 = document.createElement("option");
-    choice2.innerHTML = "test2";
-    choice2.id = "TEST2";
-    dropDown.append(choice2);
+    choice = document.createElement("option");
+    choice.innerHTML = "Tag Bygg";
+    choice.setAttribute("id", "Bygg");
+    dropDown.append(choice);
 
+    choice = document.createElement("option");
+    choice.innerHTML = "Tag Lego";
+    choice.id = "Lego";
+    dropDown.append(choice);
 
-    divShop.append(dropDown);
+    divMain.append(dropDown);
 
+    //shop
+    let divShop = document.createElement("div");
+    divShop.classList = "shop";
+    divMain.append(divShop);
 
     //append all
-    body.append(divShop);
+    body.append(divMain);
 
+    //uppdate
+    UppdateView(products);
 }
 
 //run
